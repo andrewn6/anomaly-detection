d6d2c76c34db33cbfddf77a1f6f63a40c7ae4aef
andrew <andrewnijmeh1@gmail.com>
2023-04-13 13:27:12 -0400

    support geojson

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# anomaly-detection
+# Anomaly Detection
+
+Hi! This is a project I built for my end of year CS project.
+
+## Why
+
+So one day I was bored and I came across opendata toronto, and they have 400+ datasets for a bunch of different things in the city (eg mobility, climate change, and misc stuff). I wanted to build something with one of these data sets, so I decided to build an Anomaly Detection program to detect if the average speed of vehicles in school zones go over the legal limit.
+
+## Running
+
+Install the deps
+
+```pip install -r requirements.txt```
+
+Run streamlit
+
+```
+./run
+```
+
+After this it should open your browser with a user interface
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 import streamlit as st
 
 def detect_anomalies(df, column_name, threshold):
-  mean = np.mean(df[column_name])
-  std = np.std(df[column_name])
+    mean = np.mean(df[column_name])
+    std = np.std(df[column_name])
 
-  df['z_score'] = (df[column_name] - mean) / std
+    df['z_score'] = (df[column_name] - mean) / std
 
-  anomalies = df[np.abs(df['z_score']) > threshold]
+    anomalies = df[np.abs(df['z_score']) > threshold]
+
+    return anomalies
 
 st.title("Anomaly Detection - School Safety Zones")
 
-uploaded_file = st.file_uploader("Upload data set", type="csv")
+uploaded_file = st.file_uploader("Upload data set", type=["csv", "geojson"])
 
 DEFAULT_THRESHOLD = 2.5
 DEFAULT_COLUMN_NAME = "strobe_speed"
@@ -20,21 +23,27 @@ DEFAULT_COLUMN_NAME = "strobe_speed"
 threshold = st.sidebar.slider("Threshold", min_value=1.0, max_value=5.0, step=0.1, value=float(DEFAULT_THRESHOLD))
 
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+    if uploaded_file.type == 'text/csv':
+        df = pd.read_file(uploaded_file)
+    elif uploaded_file.type == 'application/geo+json':
+        df = gpd.read_file(uploaded_file)
+    else:
+        st.write("File type not supported. Please upload a CSV or GeoJSON file.")
+        st.stop()
 
-  column_name = st.sidebar.selectbox("Column", df.columns, index=df.column.get_loc(DEFAULT_COLUMN_NAME))
+    column_name = st.sidebar.selectbox("Column", df.columns, index=df.columns.get_loc(DEFAULT_COLUMN_NAME))
 
-  anomalies = detect_anomalies(df, column_name, threshold)
+    anomalies = detect_anomalies(df, column_name, threshold)
 
-  st.subheader("Results")
-  st.write(f"Threshold: {threshold}")
-  st.write(f"Column: {column_name}")
-  st.write(f"Number of anomalies: {len(anomalies)}")
+    st.subheader("Results")
+    st.write(f"Threshold: {threshold}")
+    st.write(f"Column: {column_name}")
+    st.write(f"Number of anomalies: {len(anomalies)}")
 
-  if not anomalies.empty:
-    st.write(anomalies)
+    if not anomalies.empty:
+        st.write(anomalies)
 
-  else:
-    st.write("No anomalies detected.")
+    else:
+        st.write("No anomalies detected.")
 else:
-  st.write("Please upload a CSV file!")
+    st.write("Please upload a CSV or GeoJSON file!")
